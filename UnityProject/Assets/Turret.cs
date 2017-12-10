020a30abfdd899adc4a1cff69b28b125287c9ed6
Yuliyan <y.georgiev@student.tue.nl>
2017-12-10 20:43:55 +0100

    Made some changes on the turret prefab

--- UnityProject/Assets/Turret.cs
+++ UnityProject/Assets/Turret.cs
@@ -5,6 +5,7 @@
  * 10.12.2017
  * Turret behaviour script
  */
+
 public class Turret : MonoBehaviour {
 
     public int range = 15;
@@ -31,15 +32,18 @@ public class Turret : MonoBehaviour {
             target = hit.rigidbody;
 
             Vector3 direction = target.position - transform.position;
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            /*Quaternion lookRotation = Quaternion.LookRotation(direction);
             Vector3 rotation = lookRotation.eulerAngles;
-            mid.rotation = Quaternion.Euler(0f, rotation.y, 0f);
-
+            mid.rotation = Quaternion.Euler(-90f, rotation.y, 0f);
+            top.rotation = Quaternion.Euler(rotation.x, 0f, 0f);
+            */
+            top.LookAt(target.position);
             nextFire = Time.time + fireRate; // Set time for the fire rate
             Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
             targetDistance = hit.distance;
             Debug.Log(target);
         }
+        Debug.DrawRay(transform.position, forward, Color.red);
 	}
 
     // Check if the raget is valid

a24284f0f3cd5518c7485d4242e2a147319e5f21
zeepNL <d.j.c.p.hiemstra@student.tue.nl>
2017-12-10 21:48:39 +0100

    Fixed rotation stuff

---
+++ UnityProject/Assets/Bullet.cs
@@ -0,0 +1,28 @@
+﻿/* Bullet.cs
+ * D.J.C.P. Hiemstra
+ * Based on Mover.cs by Yuliyan Georgiev
+ * 10-12-17
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	[SerializeField]
+	float destroyAfterThisTime;
+	public float speed;
+
+	private void Start() {
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+		Invoke("DestroyBullet", destroyAfterThisTime);
+	}
+
+	private void OnCollisionEnter(Collision c) {
+		DestroyBullet();
+	}
+
+	void DestroyBullet() {
+		Destroy(gameObject);
+	}
+}
--- UnityProject/Assets/Turret.cs
+++ UnityProject/Assets/Turret.cs
@@ -7,43 +7,79 @@
  */
 public class Turret : MonoBehaviour {
 
+	public float rotSpeed;
     public int range = 15;
-    public float targetDistance;
+    //public float targetDistance;
     private LineRenderer line;
     public Transform mid;
     public Transform top;
+	public Transform laserOrigin;
     public GameObject bulletSpawn;
     public GameObject bullet;
-    public Rigidbody target;
+	public GameObject target;
     public float fireRate;
     private float nextFire;
+	[SerializeField]
+	LineRenderer laser;
+	Quaternion lookRotation;
+	public float searchTime;
+	float stopSearchTime;
+	public Light muzzleFlash;
+	float muzzleFlashTime;
 
+	Vector3 direction;
 	void Start () {
         line = GetComponent<LineRenderer>();
 	}
 	
 	void Update () {
         RaycastHit hit;
-        Vector3 forward = transform.TransformDirection(Vector3.forward) * range;
+        //Vector3 forward = transform.TransformDirection(Vector3.forward) * range;
+		if(target != null) { // so the turret rotates to the target before it checks if it can still see it. Improves tracking.
+			direction = new Vector3(target.transform.position.x - transform.position.x, 0, target.transform.position.z - transform.position.z);
+		} else if(Time.time > stopSearchTime) {
+			if(rotSpeed == 0) {
+				direction = Quaternion.Euler(0,Time.deltaTime*100,0) * transform.forward;
+			} else {
+				direction = Quaternion.Euler(0,Time.deltaTime*rotSpeed,0) * laserOrigin.forward;
+			}
+		}
 
-        if (Physics.Raycast(transform.position, forward, out hit, range) && IsTarget(hit) && Time.time > nextFire)
-        {
-            target = hit.rigidbody;
 
-            Vector3 direction = target.position - transform.position;
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
-            Vector3 rotation = lookRotation.eulerAngles;
-            mid.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
-            nextFire = Time.time + fireRate; // Set time for the fire rate
-            Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
-            targetDistance = hit.distance;
-            Debug.Log(target);
-        }
+
+		lookRotation = Quaternion.LookRotation(direction);
+		mid.rotation = lookRotation;
+
+		if (Physics.Raycast(laserOrigin.position, laserOrigin.forward, out hit, range)) {
+			if(IsTarget(hit)) {
+				stopSearchTime = Time.time + searchTime;
+				target = hit.transform.gameObject;
+				//targetDistance = hit.distance;
+				//Debug.Log(target);
+				Fire();
+			} else {
+				target = null; // Target is lost when the turret can't see it
+
+			}
+		}
+		laser.SetPosition(1, new Vector3(0,0,hit.distance)); // Sets laser length. Too short don't know why!
+		if(Time.time > muzzleFlashTime) {
+			muzzleFlash.enabled = false;
+		}
 	}
 
-    // Check if the raget is valid
-    bool IsTarget(RaycastHit hit)
+	void Fire() {
+		if(Time.time > nextFire) {
+			nextFire = Time.time + fireRate; // Set time for the fire rate
+			Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+			muzzleFlash.enabled = true;
+			muzzleFlashTime = Time.time + 0.1f;
+		}
+	}
+
+    // Check if the target is valid
+    bool IsTarget(RaycastHit hit) // Is this still needed?
     {
         return (hit.transform.tag == "Turret" || hit.transform.tag == "Player");
     }

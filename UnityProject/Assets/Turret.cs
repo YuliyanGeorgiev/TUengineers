37c3850c2d19f59e1c04361bc6b4a313da8eb27b
Yuliyan <y.georgiev@student.tue.nl>
2017-12-10 16:53:07 +0100

    Made turret shoot bullets at Player and Turret

---
+++ UnityProject/Assets/Turret.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+/*
+ * @author Yuliyan Georgiev 
+ * 10.12.2017
+ * Turret behaviour script
+ */
+public class Turret : MonoBehaviour {
+
+    public int range = 15;
+    public float targetDistance;
+    private LineRenderer line;
+    public Transform mid;
+    public Transform top;
+    public GameObject bulletSpawn;
+    public GameObject bullet;
+    public Rigidbody target;
+    public float fireRate;
+    private float nextFire;
+
+	void Start () {
+        line = GetComponent<LineRenderer>();
+	}
+	
+	void Update () {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * range;
+
+        if (Physics.Raycast(transform.position, forward, out hit, range) && IsTarget(hit) && Time.time > nextFire)
+        {
+            target = hit.rigidbody;
+
+            Vector3 direction = target.position - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            Vector3 rotation = lookRotation.eulerAngles;
+            mid.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+            nextFire = Time.time + fireRate; // Set time for the fire rate
+            Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            targetDistance = hit.distance;
+            Debug.Log(target);
+        }
+	}
+
+    // Check if the raget is valid
+    bool IsTarget(RaycastHit hit)
+    {
+        return (hit.transform.tag == "Turret" || hit.transform.tag == "Player");
+    }
+}

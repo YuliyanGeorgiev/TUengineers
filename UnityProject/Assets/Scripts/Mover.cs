04cbae1c5b9c2c4f8e1345dd5000d6056a4bc951
zeepNL <d.j.c.p.hiemstra@student.tue.nl>
2017-12-10 21:42:44 +0100

    Fixed turret rotation stuff

---
+++ UnityProject/Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}

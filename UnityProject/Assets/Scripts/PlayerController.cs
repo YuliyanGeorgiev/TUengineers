184478751cae8e564ed1ac34723cc3f836c505eb
zeepNL <d.j.c.p.hiemstra@student.tue.nl>
2017-12-16 17:24:35 +0100

    Fixed jumping with help of Goos :D and added vertical turret tracking

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -60,10 +60,9 @@ public class PlayerController : MonoBehaviour {
 		} else {
 			run = false; //stop run animation
 		}
-		if(Input.GetKey(KeyCode.Space)) {
-			if(grounded) {
-				velocity += transform.up * jumpForce;
-			}
+		if(Input.GetKeyDown(KeyCode.Space) && grounded == true) {
+				velocity += transform.up * jumpForce * 0.5f;
+				grounded = false; 
 		}
 
 		carry = Input.GetKey(KeyCode.Mouse0); //carry = true if mousebutton is pressed
@@ -87,7 +86,7 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		grounded = Physics.Raycast(transform.position + transform.up, -transform.up, 1f);
+		grounded = Physics.Raycast(transform.position - transform.up*0.99f, -transform.up, 0.3f);
 		rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(velocity.x, rb.velocity.y + velocity.y, velocity.z), acceleration);
 		transform.Rotate(transform.up * Input.GetAxis("Mouse X") * rotSpeed);
 	}

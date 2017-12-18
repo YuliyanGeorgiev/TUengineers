dadc1f1f4e81b243d0afba9e1a7aa1a9f1f88230
Nihal11 <goelnihal@gmail.com>
2017-12-18 07:02:15 +0100

    updates

---
+++ UnityProject/Assets/Scripts/JoinGame.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class JoinGame : MonoBehaviour {
+
+	[SerializeField]
+	NetworkManager manager;
+
+	[SerializeField]
+	GameObject myCameraObject;
+
+	[SerializeField]
+	Button joinButton;
+
+	[SerializeField]
+	Canvas canvasObject;
+
+	[SerializeField]
+	Input myInput;
+
+	Camera myCamera;
+	Canvas myCanvas;
+	Button btn;
+	// Use this for initialization
+	void Start () {
+		myCamera = myCameraObject.transform.GetComponent<Camera> ();
+		btn = joinButton.GetComponent<Button> ();
+		myCanvas = canvasObject.GetComponent<Canvas> ();
+
+		btn.onClick.RemoveAllListeners ();
+		btn.onClick.AddListener(TaskOnClick);
+
+	}
+
+	void TaskOnClick (){
+		manager.networkPort = 7777;
+		manager.networkAddress = myInput.ToString();
+		manager.StartClient();
+		myCamera.enabled = false;
+		myCanvas.enabled = false;
+
+
+	}
+
+}
---
+++ UnityProject/Assets/Scripts/MenuStart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MenuStart : MonoBehaviour {
+
+	[SerializeField]
+	NetworkManager manager;
+
+	[SerializeField]
+	GameObject myCameraObject;
+
+	[SerializeField]
+	Button hostButton;
+
+	[SerializeField]
+	Canvas canvasObject;
+
+	Camera myCamera;
+	Canvas myCanvas;
+	// Use this for initialization
+	void Start () {
+		myCamera = myCameraObject.transform.GetComponent<Camera> ();
+		Button btn = hostButton.GetComponent<Button> ();
+		myCanvas = canvasObject.GetComponent<Canvas> ();
+		btn.onClick.RemoveAllListeners ();
+		btn.onClick.AddListener(TaskOnClick);
+
+	}
+
+	void TaskOnClick (){
+		myCamera.enabled = false;
+		myCanvas.enabled = false;
+		manager.networkAddress = Network.player.ipAddress;
+		manager.StartHost();
+	}
+
+}

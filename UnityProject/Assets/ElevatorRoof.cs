27edeb060d1fded45a37e946ddc8e849c07d1ddb
zeepNL <d.j.c.p.hiemstra@student.tue.nl>
2017-12-20 15:35:24 +0100

    Added elevator terminal crushing

---
+++ UnityProject/Assets/ElevatorRoof.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorRoof : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider c) {
+		if(c.transform.tag == "Turret") {
+			c.transform.GetComponent<Turret>().health = 0;
+			foreach(Transform child in c.transform.GetComponentsInChildren<Transform>()) {
+				child.gameObject.AddComponent<Rigidbody>();
+				child.gameObject.AddComponent<BoxCollider>();
+				child.parent = null;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider c) {
+		c.transform.parent = null;
+	}
+}
---
+++ UnityProject/Assets/Scripts/ElevatorFix.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider c) {
+		c.transform.parent = this.transform;
+	}
+
+	void OnTriggerExit(Collider c) {
+		c.transform.parent = null;
+	}
+}

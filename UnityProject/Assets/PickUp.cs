51eeadab65405eaf68022eb121c42f57d0bd9ead
Georgiev <y.georgiev@student.tue.nl>
2018-01-15 12:22:59 +0100

    Changed turret on EE level

---
+++ UnityProject/Assets/PickUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PickUp : MonoBehaviour, IInteractiveObject
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Interact(Transform player)
+    {
+        transform.parent = player;
+        transform.position = player.position + player.transform.forward;
+        transform.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void Release()
+    {
+        transform.parent = null;
+        transform.GetComponent<Rigidbody>().isKinematic = false;
+    }
+}

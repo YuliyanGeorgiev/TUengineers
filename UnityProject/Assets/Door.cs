704c65748349e1153d1c9ad48f10342c546b8643
Georgiev <y.georgiev@student.tue.nl>
2018-01-15 11:27:38 +0100

    EE level working

---
+++ UnityProject/Assets/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Socket input;
+
+    void Update()
+    {
+        if (input.output)
+        {
+            Open();
+        }
+        else
+        {
+            Close();
+        }
+    }
+
+    void Open()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1, 1, 0.2f), Time.deltaTime);
+    }
+
+    void Close()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1, 1, 1), Time.deltaTime);
+    }
+}
---
+++ UnityProject/Assets/Socket.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Socket : MonoBehaviour
+{
+
+    public Socket input1;
+    public Socket input2;
+    public bool output;
+    public GameObject outputWire;
+    GameObject gate;
+    public bool isCable;
+
+
+    void OnTriggerEnter(Collider c)
+    {
+        gate = c.gameObject;
+    }
+
+    void Update()
+    {
+        if (isCable)
+        {
+            return;
+        }
+
+        if (gate != null)
+        {
+            if (gate.name == "AND")
+            {
+                if (input1.output && input2.output)
+                {
+                    output = true;
+                }
+                else
+                {
+                    output = false;
+                }
+            }
+            if (gate.name == "XOR")
+            {
+                if (input1.output ^ input2.output)
+                {
+                    output = true;
+                }
+                else
+                {
+                    output = false;
+                }
+            }
+            if (gate.name == "INV")
+            {
+                output = !input1;
+            }
+        }
+        else
+        {
+            output = false;
+        }
+
+        if (output)
+        {
+            outputWire.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+            foreach (Transform child in outputWire.transform)
+            {
+                child.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }
+        else
+        {
+            outputWire.transform.GetComponent<MeshRenderer>().material.color = Color.black;
+            foreach (Transform child in outputWire.transform)
+            {
+                child.transform.GetComponent<MeshRenderer>().material.color = Color.black;
+            }
+        }
+    }
+
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.name == "AND" || c.name == "XOR" || c.name == "INV")
+        {
+            gate = null;
+            output = false;
+        }
+    }
+}

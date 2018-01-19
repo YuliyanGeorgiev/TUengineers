63e66da78ff7a64a4e62ecc9dbe6a4442c11bbbe
Nihal11 <goelnihal@gmail.com>
2018-01-19 16:27:07 +0100

    update branch

--- UnityProject/Assets/Scripts/Instructions.cs
+++ UnityProject/Assets/Scripts/Instructions.cs
@@ -19,9 +19,12 @@ public class Instructions : MonoBehaviour {
 	[SerializeField]
 	GameObject InstructionMenu;
 
+	bool setBool;
+
 	// Use this for initialization
 	void Start () {
 		Button btn = instructionButton.GetComponent<Button> ();
+		setBool = false;
 		btn.onClick.RemoveAllListeners ();
 		btn.onClick.AddListener(TaskOnClick);
 
@@ -30,14 +33,16 @@ public class Instructions : MonoBehaviour {
 	void TaskOnClick (){
 		canvasObject.GetComponent<Canvas>().enabled = false;
 		sciencePark.GetComponent<MeshRenderer>().enabled = false;
+		setBool = true;
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.Escape) && InstructionMenu.GetComponent<MeshRenderer>().isVisible) {
+		if (Input.GetKey (KeyCode.Escape) && setBool) {
 			canvasObject.GetComponent<Canvas>().enabled = true;
 			sciencePark.GetComponent<MeshRenderer> ().enabled = true;
+			setBool = false;
 		}
 	}
 }
